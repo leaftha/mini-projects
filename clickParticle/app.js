081737e74d93ed10af1e260b175a536e33c210f4
SaChan <vltpcks@gmail.com>
2023-02-25 22:20:43 +0900

    mini-project ClickParticle canvas size setting

--- clickParticle/app.js
+++ clickParticle/app.js
@@ -1,2 +1,23 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const dpr = window.devicePixelRatio;
+
+function init() {
+  let canvasWidth = innerWidth;
+  let canvasHeight = innerHeight;
+
+  canvas.style.width = `${canvasWidth}px`;
+  canvas.style.height = `${canvasHeight}px`;
+
+  canvas.width = canvasWidth * dpr;
+  canvas.height = canvasHeight * dpr;
+  ctx.scale(dpr, dpr);
+}
+
+window.addEventListener("load", () => {
+  init();
+});
+
+window.addEventListener("resize", () => {
+  init();
+});

5cb68adbe2466056739f00cd78fc33296f8f4255
SaChan <vltpcks@gmail.com>
2023-02-26 17:07:59 +0900

    mini-project ClickParticle move the particle and bounce the wall

--- clickParticle/Particle.js
+++ clickParticle/Particle.js
@@ -1,15 +1,27 @@
+import { randomNumBetween } from "./utils.js";
+
 export default class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+
+    this.r = randomNumBetween(0, 255);
+    this.g = randomNumBetween(0, 255);
+    this.b = randomNumBetween(0, 255);
+
+    this.vx = randomNumBetween(-20, 20);
+    this.vy = randomNumBetween(-20, 20);
   }
 
-  update() {}
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
 
   draw(ctx) {
     ctx.beginPath();
-    ctx.fillStyle = `#fff`;
-    ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
+    ctx.fillStyle = `rgb(${this.r},${this.b},${this.g})`;
+    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
   }
--- clickParticle/app.js
+++ clickParticle/app.js
@@ -7,11 +7,10 @@ const dpr = window.devicePixelRatio;
 let interval = 1000 / 60;
 
 let particles = [];
+let canvasWidth = innerWidth;
+let canvasHeight = innerHeight;
 
 function init() {
-  let canvasWidth = innerWidth;
-  let canvasHeight = innerHeight;
-
   canvas.style.width = `${canvasWidth}px`;
   canvas.style.height = `${canvasHeight}px`;
 
@@ -39,9 +38,19 @@ function render() {
     delta = now - then;
     if (delta < interval) return;
 
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
     for (let i = 0; i < particles.length; i++) {
       particles[i].update();
       particles[i].draw(ctx);
+
+      if (particles[i].x > canvasWidth - 50 || particles[i].x < 0) {
+        particles[i].vx *= -1;
+      }
+
+      if (particles[i].y > canvasHeight - 50 || particles[i].y < 0) {
+        particles[i].vy *= -1;
+      }
     }
 
     then = now - (delta % interval);

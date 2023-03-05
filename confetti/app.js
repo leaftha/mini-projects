5cb996483e7d6bddb6496c470166808239d5ea9f
SaChan <vltpcks@gmail.com>
2023-03-05 14:59:49 +0900

    mini-project (Confetti) canvas size

--- confetti/app.js
+++ confetti/app.js
@@ -1,2 +1,22 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+const dpr = window.devicePixelRatio > 1 ? 2 : 1;
+let canvasWidth = innerWidth;
+let canvasHeight = innerHeight;
+
+function init() {
+  canvasWidth = innerWidth;
+  canvasHeight = innerHeight;
+  canvas.style.width = `${canvasWidth}px`;
+  canvas.style.height = `${canvasHeight}px`;
+  canvas.width = canvasWidth * dpr;
+  canvas.height = canvasHeight * dpr;
+  ctx.scale(dpr, dpr);
+}
+
+window.addEventListener("load", () => {
+  init();
+});
+
+window.addEventListener("resize", init);

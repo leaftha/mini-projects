31ad5135cd43a2edffcd3315d7da2b6826cab6df
SaChan <vltpcks@gmail.com>
2023-03-10 21:40:46 +0900

    mini-project (Rope) canvas Stick connect

--- rope/js/App.js
+++ rope/js/App.js
@@ -17,10 +17,14 @@ export default class App {
     this.dots = [
       new Dot(500, 100),
       new Dot(600, 100),
-      new Dot(500, 100),
-      new Dot(600, 100),
+      new Dot(700, 100),
+      new Dot(800, 100),
     ];
-    this.sticks = [new Stick(this.dots[0], this.dots[1])];
+    this.sticks = [
+      new Stick(this.dots[0], this.dots[1]),
+      new Stick(this.dots[1], this.dots[2]),
+      new Stick(this.dots[2], this.dots[3]),
+    ];
     this.dots[0].pinned = true;
   }
 
--- rope/js/Stick.js
+++ rope/js/Stick.js
@@ -2,9 +2,29 @@ export default class Stick {
   constructor(p1, p2) {
     this.startPoint = p1;
     this.endPoint = p2;
+    this.length = this.startPoint.pos.dist(this.endPoint.pos);
+    this.tension = 0.4;
   }
 
-  update() {}
+  update() {
+    const dx = this.endPoint.pos.x - this.startPoint.pos.x;
+    const dy = this.endPoint.pos.y - this.startPoint.pos.y;
+
+    const dist = Math.sqrt(dx * dx + dy * dy);
+    const diff = (dist - this.length) / dist;
+
+    const ax = diff * dx * this.tension;
+    const ay = diff * dy * this.tension;
+
+    if (!this.startPoint.pinned) {
+      this.startPoint.pos.x += ax;
+      this.startPoint.pos.y += ay;
+    }
+    if (!this.endPoint.pinned) {
+      this.endPoint.pos.x -= ax;
+      this.endPoint.pos.y -= ay;
+    }
+  }
 
   draw(ctx) {
     ctx.beginPath();

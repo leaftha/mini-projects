bb4aa3b512114073e0169b25942439544908c3a1
leaftha <vltpcks@gmail.com>
2023-07-12 10:15:24 +0900

    canvas reszie

--- react-confetti/src/confetti.js
+++ react-confetti/src/confetti.js
@@ -6,6 +6,21 @@ const Confetti = () => {
         const canvas = canvasref.current;
         const canvasParent = canvas.parentNode;
         const ctx = canvas.getContext('2d');
+        const dpr = window.devicePixelRatio > 1 ? 2 : 1;
+
+        let canvasWidth, canvasHeight;
+
+        function resize() {
+            canvasWidth = canvasParent.clientWidth;
+            canvasHeight = canvasParent.clientHeight;
+            canvas.style.width = canvasWidth + 'px';
+            canvas.style.height = canvasHeight + 'px';
+            canvas.width = canvasWidth;
+            canvas.height = canvasHeight;
+            ctx.scale(dpr, dpr);
+        }
+        window.addEventListener('resize', resize);
+        resize();
     }, []);
     return <canvas ref={canvasref} />;
 };

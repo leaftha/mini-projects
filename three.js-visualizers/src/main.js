c5808f00aa3ef62e8886b40ed4d313e716ee51e8
SaChan <vltpcks@gmail.com>
2023-08-02 21:46:30 +0900

    Experiment

--- three.js-visualizers/src/main.js
+++ three.js-visualizers/src/main.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function () {
 
 function init() {
   const renderer = new THREE.WebGLRenderer({
-    // alpha: true,
+    alpha: true,
     antialias: true,
   });
   document.body.appendChild(renderer.domElement);
@@ -23,7 +23,7 @@ function init() {
     500
   );
 
-  camera.position.z = 5;
+  camera.position.z = 10;
 
   const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -33,43 +33,88 @@ function init() {
   const sound = new THREE.Audio(listener);
   const audioLoader = new THREE.AudioLoader();
 
-  audioLoader.load("sounds/sound1.mp3", function (buffer) {
-    sound.setBuffer(buffer);
-    sound.setLoop(true);
-    sound.setVolume(0.5);
-    sound.play();
+  audioLoader.load(
+    "sounds/Billy_Joel_-_Piano_Man_(Jesusful.com).mp3",
+    function (buffer) {
+      sound.setBuffer(buffer);
+      sound.setLoop(true);
+      sound.setVolume(0.5);
+      sound.play();
+    }
+  );
+
+  camera.position.set(0, 25, 150);
+  const waveGeometry = new THREE.PlaneGeometry(1500, 1500, 150, 150);
+  const waveMaterial = new THREE.MeshStandardMaterial({
+    wireframe: true,
+    color: 0x111111,
   });
 
-  const Boxgeometry = new THREE.BoxGeometry(2, 2, 2);
-  const Boxmaterial = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+  const wave = new THREE.Mesh(waveGeometry, waveMaterial);
 
-  const Boxmesh = new THREE.Mesh(Boxgeometry, Boxmaterial);
-  scene.add(Boxmesh);
+  wave.rotation.x = -Math.PI / 2;
+  wave.receiveShadow = true;
+  const waveHeight = 1;
+
+  const initialZPositions = [];
+
+  for (let i = 0; i < waveGeometry.attributes.position.count; i++) {
+    const z = waveGeometry.attributes.position.getZ(i);
+    initialZPositions.push(z);
+  }
+
+  const clock = new THREE.Clock();
+  let count = 0;
+  wave.update = function (data) {
+    const elaspedTime = clock.getElapsedTime();
+    for (let i = 0; i < waveGeometry.attributes.position.count; i++) {
+      let z =
+        initialZPositions[i] +
+        Math.sin(elaspedTime * 3 + i ** 2) * data[count] * 0.03;
+      if (count < 9) {
+        count++;
+      } else {
+        count = 0;
+      }
+      waveGeometry.attributes.position.setZ(i, z);
+    }
+
+    waveGeometry.attributes.position.needsUpdate = true;
+  };
+
+  scene.add(wave);
+
+  // const Boxgeometry = new THREE.BoxGeometry(2, 2, 2);
+  // const Boxmaterial = new THREE.MeshLambertMaterial({ color: 0x00ffff });
+
+  // const Boxmesh = new THREE.Mesh(Boxgeometry, Boxmaterial);
+  // scene.add(Boxmesh);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+
+  scene.add(ambientLight);
 
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 
-  directionalLight.position.set(-1, 2, 3);
+  directionalLight.position.set(3, 2, 2);
 
   scene.add(directionalLight);
-  // create an AudioAnalyser, passing in the sound and desired fftSize
   const analyser = new THREE.AudioAnalyser(sound, 32);
-
-  // get the average frequency of the sound
-
-  const clock = new THREE.Clock();
 
   render();
 
   function render() {
-    const elapsedTime = clock.getElapsedTime();
     renderer.render(scene, camera);
     const data = analyser.getAverageFrequency();
+    const arr = [...analyser.data];
     // Boxmesh.rotation.x += data * 0.001;
     // Boxmesh.rotation.y += data * 0.001;
     // Boxmesh.rotation.z += data * 0.001;
-    Boxmesh.scale.x = 1 + data * 0.01;
-    Boxmesh.scale.y = 1 + data * 0.01;
-    Boxmesh.scale.z = 1 + data * 0.01;
+    // console.log(analyser.data, data);
+    // Boxmesh.scale.x = 1 + data * 0.01;
+    // Boxmesh.scale.y = 1 + data * 0.01;
+    // Boxmesh.scale.z = 1 + data * 0.01;
+    wave.update(arr);
     requestAnimationFrame(render);
   }
 

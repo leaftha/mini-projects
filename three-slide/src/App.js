2c0e31ef263a6b411471054b798f633cf0a2bfdd
SaChan <vltpcks@gmail.com>
2024-02-29 19:54:01 +0900

    btn rotation

--- three-slide/src/App.js
+++ three-slide/src/App.js
@@ -1,20 +1,31 @@
-import './App.css';
-import { Canvas } from '@react-three/fiber';
-import Plane from './componencts/Plane';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Plane from "./componencts/Plane";
+import { useState } from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <Canvas>
-                <Plane num={1} />
-                <Plane num={2} />
-                <Plane num={3} />
-                <Plane num={4} />
-                <Plane num={5} />
-                <Plane num={6} />
-            </Canvas>
-        </div>
-    );
+  const [rotationAngle, setRotationAngle] = useState(0);
+
+  const Nextrortaion = () => {
+    setRotationAngle(rotationAngle + 1.05);
+  };
+  const Prevrortaion = () => {
+    setRotationAngle(rotationAngle - 1.05);
+  };
+  return (
+    <div className="App">
+      <button onClick={Prevrortaion}>prev</button>
+      <button onClick={Nextrortaion}>next</button>
+      <Canvas>
+        <Plane num={1} rotationAngle={rotationAngle} />
+        <Plane num={2} rotationAngle={rotationAngle} />
+        <Plane num={3} rotationAngle={rotationAngle} />
+        <Plane num={4} rotationAngle={rotationAngle} />
+        <Plane num={5} rotationAngle={rotationAngle} />
+        <Plane num={6} rotationAngle={rotationAngle} />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
--- three-slide/src/componencts/Plane.js
+++ three-slide/src/componencts/Plane.js
@@ -1,30 +1,36 @@
-import { useFrame, useThree } from '@react-three/fiber';
-import { useRef, useState } from 'react';
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef, useState } from "react";
 
-function Plane({ num }) {
-    const refMesh = useRef();
-    const { camera } = useThree();
-    const radius = 50;
-    const angleStep = (Math.PI * 2) / 6;
-    let angle = angleStep * num + 0.5;
+function Plane({ num, rotationAngle }) {
+  const refMesh = useRef();
+  const lightMesh = useRef();
+  const { camera } = useThree();
+  const radius = 100;
+  const angleStep = (Math.PI * 2) / 6;
 
-    useFrame((state, delta) => {
-        // angle += delta;
-        const x = Math.cos(angle) * radius + camera.position.x;
-        const z = Math.sin(angle) * radius + camera.position.z;
-        refMesh.current.position.set(x, 0, z);
-        refMesh.current.lookAt(camera.position);
-    });
+  const [currentAngle, setCurrentAngle] = useState(angleStep * num + 0.5);
 
-    return (
-        <>
-            <directionalLight position={[1, 1, 1]} />
-            <mesh ref={refMesh} scale={3}>
-                <planeGeometry args={[10, 10, 10]} />
-                <meshStandardMaterial color={'#F2BED1'} />
-            </mesh>
-        </>
-    );
+  useFrame((state, delta) => {
+    const targetAngle = angleStep * num + 0.5 + rotationAngle;
+    const newAngle = currentAngle + (targetAngle - currentAngle) * 0.05;
+
+    const x = Math.cos(newAngle) * radius + camera.position.x;
+    const z = Math.sin(newAngle) * radius + camera.position.z;
+    refMesh.current.position.set(x, 0, z);
+    refMesh.current.lookAt(camera.position);
+
+    setCurrentAngle(newAngle);
+  });
+
+  return (
+    <>
+      <directionalLight ref={lightMesh} position={[0, 1, 1]} />
+      <mesh ref={refMesh} scale={3}>
+        <planeGeometry args={[20, 20]} />
+        <meshStandardMaterial color={"#F2BED1"} />
+      </mesh>
+    </>
+  );
 }
 
 export default Plane;

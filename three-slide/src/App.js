dec5a990b296e2e62165ee06c8aebe8c4ee18d90
SaChan <vltpcks@gmail.com>
2024-02-29 20:31:28 +0900

    change angle

--- three-slide/src/App.js
+++ three-slide/src/App.js
@@ -7,15 +7,16 @@ function App() {
   const [rotationAngle, setRotationAngle] = useState(0);
 
   const Nextrortaion = () => {
-    setRotationAngle(rotationAngle + 1.05);
+    setRotationAngle(rotationAngle - 1.0471975512);
   };
   const Prevrortaion = () => {
-    setRotationAngle(rotationAngle - 1.05);
+    setRotationAngle(rotationAngle + 1.0471975512);
   };
   return (
     <div className="App">
       <button onClick={Prevrortaion}>prev</button>
       <button onClick={Nextrortaion}>next</button>
+
       <Canvas>
         <Plane num={1} rotationAngle={rotationAngle} />
         <Plane num={2} rotationAngle={rotationAngle} />
--- three-slide/src/componencts/Plane.js
+++ three-slide/src/componencts/Plane.js
@@ -13,7 +13,6 @@ function Plane({ num, rotationAngle }) {
   useFrame((state, delta) => {
     const targetAngle = angleStep * num + 0.5 + rotationAngle;
     const newAngle = currentAngle + (targetAngle - currentAngle) * 0.05;
-
     const x = Math.cos(newAngle) * radius + camera.position.x;
     const z = Math.sin(newAngle) * radius + camera.position.z;
     refMesh.current.position.set(x, 0, z);

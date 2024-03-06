687a96cfe3a76f2e7bb04feb3874c9c360353767
SaChan <vltpcks@gmail.com>
2024-03-06 21:05:34 +0900

    primitive setting

--- three-slide/src/componencts/Object.js
+++ three-slide/src/componencts/Object.js
@@ -1,35 +1,77 @@
-import { Environment, useGLTF } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
-import { useEffect, useRef, useState } from 'react';
+import { Environment, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
 function Object({ num }) {
-    const model = useGLTF(`./models/${num}.glb`);
-    const refMesh = useRef();
+  const model1 = useGLTF(`./models/1.glb`);
+  const model2 = useGLTF(`./models/2.glb`);
+  const model3 = useGLTF(`./models/3.glb`);
+  const model4 = useGLTF(`./models/4.glb`);
+  const model5 = useGLTF(`./models/5.glb`);
+  const model6 = useGLTF(`./models/6.glb`);
+  const refMesh1 = useRef();
+  const refMesh2 = useRef();
+  const refMesh3 = useRef();
+  const refMesh4 = useRef();
+  const refMesh5 = useRef();
+  const refMesh6 = useRef();
 
-    useFrame((state, delta) => {
-        refMesh.current.rotation.y += delta;
-    });
-    let modalScale = 1;
-    // console.log(refMesh.current);
-    if (num === 4) {
-        modalScale = 0.2;
-    } else if (num === 5) {
-        modalScale = 0.02;
-    } else if (num === 6) {
-        modalScale = 0.03;
-    } else if (num === 1) {
-        modalScale = 2;
-    } else if (num === 2) {
-        modalScale = 0.3;
-    } else if (num === 3) {
-        modalScale = 0.2;
-    }
+  useFrame((state, delta) => {
+    // refMesh1.current.rotation.y += delta;
+  });
 
-    return (
-        <>
-            {/* <Environment preset="sunset" /> */}
-            <primitive ref={refMesh} scale={modalScale} position-y={-2.5} object={model.scene} />
-        </>
-    );
+  //   // 반투명 효과를 위해 모델의 material을 반복하여 수정
+  //   useEffect(() => {
+  //     if (refMesh1.current) {
+  //       refMesh1.current.traverse((child) => {
+  //         if (child.isMesh) {
+  //           child.material.transparent = true;
+  //           child.material.opacity = 1;
+  //         }
+  //       });
+  //     }
+  //   }, [num]);
+
+  return (
+    <>
+      {/* <Environment preset="sunset" /> */}
+      {/* <primitive
+        ref={refMesh1}
+        scale={2}
+        position-y={-3}
+        object={model1.scene}
+      /> */}
+      {/* <primitive
+        ref={refMesh2}
+        scale={0.5}
+        position-y={-2}
+        object={model2.scene}
+      /> */}
+      {/* <primitive
+        ref={refMesh3}
+        scale={0.2}
+        position-y={-2}
+        object={model3.scene}
+      /> */}
+      {/* <primitive
+        ref={refMesh4}
+        scale={0.15}
+        position-y={-2.4}
+        object={model4.scene}
+      /> */}
+      {/* <primitive
+        ref={refMesh5}
+        scale={0.015}
+        position-y={-3}
+        object={model5.scene}
+      /> */}
+      {/* <primitive
+        ref={refMesh6}
+        scale={0.03}
+        position-y={-3}
+        object={model6.scene}
+      /> */}
+    </>
+  );
 }
 
 export default Object;

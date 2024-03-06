6a7d2ba504844092766e71cf937204cf255afcbe
SaChan <vltpcks@gmail.com>
2024-03-06 21:29:13 +0900

    models opacity

--- three-slide/src/componencts/Object.js
+++ three-slide/src/componencts/Object.js
@@ -19,57 +19,94 @@ function Object({ num }) {
     // refMesh1.current.rotation.y += delta;
   });
 
-  //   // 반투명 효과를 위해 모델의 material을 반복하여 수정
-  //   useEffect(() => {
-  //     if (refMesh1.current) {
-  //       refMesh1.current.traverse((child) => {
-  //         if (child.isMesh) {
-  //           child.material.transparent = true;
-  //           child.material.opacity = 1;
-  //         }
-  //       });
-  //     }
-  //   }, [num]);
+  // 반투명 효과를 위해 모델의 material을 반복하여 수정
+  useEffect(() => {
+    if (
+      refMesh1.current &&
+      refMesh2.current &&
+      refMesh3.current &&
+      refMesh4.current &&
+      refMesh5.current &&
+      refMesh6.current
+    ) {
+      refMesh1.current.traverse((child) => {
+        if (child.isMesh) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+      });
+      refMesh2.current.traverse((child) => {
+        if (child.isMesh) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+      });
+      refMesh3.current.traverse((child) => {
+        if (child.isMesh) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+      });
+      refMesh4.current.traverse((child) => {
+        if (child.isMesh) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+      });
+      refMesh5.current.traverse((child) => {
+        if (child.isMesh) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+      });
+      refMesh6.current.traverse((child) => {
+        if (child.isMesh) {
+          child.material.transparent = true;
+          child.material.opacity = 0;
+        }
+      });
+    }
+  }, [num]);
 
   return (
     <>
       {/* <Environment preset="sunset" /> */}
-      {/* <primitive
+      <primitive
         ref={refMesh1}
         scale={2}
         position-y={-3}
         object={model1.scene}
-      /> */}
-      {/* <primitive
+      />
+      <primitive
         ref={refMesh2}
         scale={0.5}
         position-y={-2}
         object={model2.scene}
-      /> */}
-      {/* <primitive
+      />
+      <primitive
         ref={refMesh3}
         scale={0.2}
         position-y={-2}
         object={model3.scene}
-      /> */}
-      {/* <primitive
+      />
+      <primitive
         ref={refMesh4}
         scale={0.15}
         position-y={-2.4}
         object={model4.scene}
-      /> */}
-      {/* <primitive
+      />
+      <primitive
         ref={refMesh5}
         scale={0.015}
         position-y={-3}
         object={model5.scene}
-      /> */}
-      {/* <primitive
+      />
+      <primitive
         ref={refMesh6}
         scale={0.03}
         position-y={-3}
         object={model6.scene}
-      /> */}
+      />
     </>
   );
 }

b3984a8628b700a12d98403b2bc85662dabcc354
leaftha <vltpcks@gmail.com>
2024-03-06 14:25:00 +0900

    error

--- three-slide/src/componencts/Object.js
+++ three-slide/src/componencts/Object.js
@@ -1,40 +1,35 @@
-import { Environment, useGLTF } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
+import { Environment, useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
 function Object({ num }) {
-  const model = useGLTF(`./models/${num}.glb`);
-  const refMesh = useRef();
+    const model = useGLTF(`./models/${num}.glb`);
+    const refMesh = useRef();
 
-  useFrame((state, delta) => {
-    refMesh.current.rotation.y += delta;
-  });
-  let modalScale = 1;
+    useFrame((state, delta) => {
+        refMesh.current.rotation.y += delta;
+    });
+    let modalScale = 1;
+    // console.log(refMesh.current);
+    if (num === 4) {
+        modalScale = 0.2;
+    } else if (num === 5) {
+        modalScale = 0.02;
+    } else if (num === 6) {
+        modalScale = 0.03;
+    } else if (num === 1) {
+        modalScale = 2;
+    } else if (num === 2) {
+        modalScale = 0.3;
+    } else if (num === 3) {
+        modalScale = 0.2;
+    }
 
-  if (num === 4) {
-    modalScale = 0.2;
-  } else if (num === 5) {
-    modalScale = 0.02;
-  } else if (num === 6) {
-    modalScale = 0.03;
-  } else if (num === 1) {
-    modalScale = 2;
-  } else if (num === 2) {
-    modalScale = 0.3;
-  } else if (num === 3) {
-    modalScale = 0.2;
-  }
-
-  return (
-    <>
-      {/* <Environment preset="sunset" /> */}
-      <primitive
-        ref={refMesh}
-        scale={modalScale}
-        position-y={-2.5}
-        object={model.scene}
-      />
-    </>
-  );
+    return (
+        <>
+            {/* <Environment preset="sunset" /> */}
+            <primitive ref={refMesh} scale={modalScale} position-y={-2.5} object={model.scene} />
+        </>
+    );
 }
 
 export default Object;

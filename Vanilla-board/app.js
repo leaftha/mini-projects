482c5987323b1cab182f2bcc3d3437b00b749274
SaChan <vltpcks@gmail.com>
2024-03-12 21:41:11 +0900

    load json file and append li

--- Vanilla-board/app.js
+++ Vanilla-board/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const ul = document.querySelector(".lists");
+fetch("./MOCK_DATA.json")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i of data) {
+      let list = document.createElement("li");
+      list.innerHTML = `${i.id}`;
+      ul.appendChild(list);
+    }
+    console.log(data);
+  })
+  .catch((err) => console.err("error"));

8bcfb600753319049e8543a9767968992ca1a7e3
leaftha <vltpcks@gmail.com>
2024-03-13 13:19:27 +0900

    add email and category

--- Vanilla-board/app.js
+++ Vanilla-board/app.js
@@ -1,12 +1,12 @@
-const ul = document.querySelector(".lists");
-fetch("./MOCK_DATA.json")
-  .then((response) => response.json())
-  .then((data) => {
-    for (let i of data) {
-      let list = document.createElement("li");
-      list.innerHTML = `${i.id}`;
-      ul.appendChild(list);
-    }
-    console.log(data);
-  })
-  .catch((err) => console.err("error"));
+const ul = document.querySelector('.lists');
+fetch('./MOCK_DATA.json')
+    .then((response) => response.json())
+    .then((data) => {
+        for (let i of data) {
+            let list = document.createElement('li');
+            list.innerHTML = `${i.id} - ${i.email} - ${i.category}`;
+            ul.appendChild(list);
+        }
+        console.log(data);
+    })
+    .catch((err) => console.err('error'));

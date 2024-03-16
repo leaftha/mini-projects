e181c335d791c0268363d7df234c2640c08d4f92
SaChan <vltpcks@gmail.com>
2024-03-16 22:37:05 +0900

    change category

--- Vanilla-board/app.js
+++ Vanilla-board/app.js
@@ -1,23 +1,22 @@
 const ul = document.querySelector(".lists");
 const select = document.querySelector(".select");
+let data = []; // 데이터를 전역 변수로 저장하여 나중에 필터링에 사용
+
 fetch("./MOCK_DATA.json")
   .then((response) => response.json())
-  .then((data) => {
+  .then((jsonData) => {
+    data = [...jsonData];
+    data = jsonData; // 데이터를 전역 변수에 저장
     let categorys = new Set();
-    for (let user of data) {
-      // 카테고리 중복없이 Set에 넣기
+    for (let user of jsonData) {
       let userCategory = user.category.split("|");
       userCategory.map((item) => {
-        categorys.add(item);
+        categorys.add(item.trim()); // trim()을 사용하여 공백을 제거하여 정확한 비교를 보장
       });
-
-      //list만들고 ul태그에 넣기
       let list = document.createElement("li");
-      list.innerHTML = `${user.id} - ${user.email}`;
+      list.innerHTML = `${user.id} - ${user.email} - ${user.category}`;
       ul.appendChild(list);
     }
-
-    // 카테고리를 select 태그에 option 태그로 넣기
     for (let category of categorys) {
       let option = document.createElement("option");
       option.setAttribute("value", category);
@@ -26,3 +25,36 @@ fetch("./MOCK_DATA.json")
     }
   })
   .catch((err) => console.error(err));
+
+select.addEventListener("change", (e) => {
+  let value = select.value;
+  let categoryData = [];
+
+  // ul내의 li들 삭제
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+
+  // Any가 아닌 선택지 일 경우 해당 카테고리가 존재하는 데이터만 배열에 저장
+  // list dom 생성 추가
+  if (value != "Any") {
+    data.map((item) => {
+      let itemCategory = item.category.split("|");
+      if (itemCategory.includes(value)) {
+        categoryData.push(item);
+      }
+    });
+
+    for (let user of categoryData) {
+      let list = document.createElement("li");
+      list.innerHTML = `${user.id} - ${user.email} - ${user.category}`;
+      ul.appendChild(list);
+    }
+  } else {
+    for (let user of data) {
+      let list = document.createElement("li");
+      list.innerHTML = `${user.id} - ${user.email} - ${user.category}`;
+      ul.appendChild(list);
+    }
+  }
+});

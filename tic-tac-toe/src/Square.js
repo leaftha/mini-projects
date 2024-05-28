2e88e92bd4c00658429da0d33bd594f207fbadd8
SaChan <vltpcks@gmail.com>
2024-05-28 18:06:28 +0900

    prop 대신 usestate사용

--- tic-tac-toe/src/Square.js
+++ tic-tac-toe/src/Square.js
@@ -1,3 +1,14 @@
-export default function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from "react";
+
+export default function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button onClick={handleClick} className="square">
+      {value}
+    </button>
+  );
 }

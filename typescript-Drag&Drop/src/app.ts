bfe8704a8911a5de2cf1b7adee661b8ed05daa3d
SaChan <vltpcks@gmail.com>
2024-08-11 16:01:12 +0900

    재사용이 가능한 검증 기능 추가

--- typescript-Drag&Drop/src/app.ts
+++ typescript-Drag&Drop/src/app.ts
@@ -1,5 +1,49 @@
-// asutobind decorator
+//vaidate
+interface Validateble {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
 
+function validate(vaidatableInput: Validateble) {
+  let isValid = true;
+
+  if (vaidatableInput.required) {
+    isValid = isValid && vaidatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    vaidatableInput.minLength != null &&
+    typeof vaidatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && vaidatableInput.value.length > vaidatableInput.minLength;
+  }
+  if (
+    vaidatableInput.maxLength != null &&
+    typeof vaidatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && vaidatableInput.value.length < vaidatableInput.maxLength;
+  }
+  if (
+    vaidatableInput.min != null &&
+    typeof vaidatableInput.value === "number"
+  ) {
+    isValid = isValid && vaidatableInput.value > vaidatableInput.min;
+  }
+  if (
+    vaidatableInput.max != null &&
+    typeof vaidatableInput.value === "number"
+  ) {
+    isValid = isValid && vaidatableInput.value < vaidatableInput.max;
+  }
+  return isValid;
+}
+
+// asutobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDesciptor: PropertyDescriptor = {
@@ -51,10 +95,25 @@ class ProjectInput {
     const enterdDescription = this.descriptionInputElement.value;
     const enterdPeople = this.peopleInputElement.value;
 
+    const titleValidatable: Validateble = {
+      value: enterdTilte,
+      required: true,
+    };
+    const descriptorValidatable: Validateble = {
+      value: enterdDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validateble = {
+      value: +enterdPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
     if (
-      enterdTilte.trim().length === 0 ||
-      enterdDescription.trim().length === 0 ||
-      enterdPeople.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(peopleValidatable) ||
+      !validate(descriptorValidatable)
     ) {
       alert("빈칸 존재");
       return;

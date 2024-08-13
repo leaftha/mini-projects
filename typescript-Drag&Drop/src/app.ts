08e7b24648173e0b444da92ec8a3fe9358c5936e
SaChan <vltpcks@gmail.com>
2024-08-13 21:06:29 +0900

    더 많은 클래스 및 사용자 정의 타입

--- typescript-Drag&Drop/src/app.ts
+++ typescript-Drag&Drop/src/app.ts
@@ -1,6 +1,23 @@
+enum ProjectStatus {
+  Acive,
+  Finished,
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
+type Listener = (items: Project[]) => void;
+
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {}
@@ -13,17 +30,18 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenersFn: Function) {
+  addListener(listenersFn: Listener) {
     this.listeners.push(listenersFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      people: numOfPeople,
-    };
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numOfPeople,
+      ProjectStatus.Acive
+    );
 
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
@@ -96,7 +114,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProject: any;
+  assignedProject: Project[];
 
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
@@ -112,7 +130,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${type}-projects`;
 
-    projectState.addListener((projects: any) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProject = projects;
       this.renderProject();
     });
